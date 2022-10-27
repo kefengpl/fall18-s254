89cb79bf7cbae3135646e5c33bae64c3cefa59fc
kefengpl <1335508895@qq.com>
2022-10-27 20:34:15 +0800

    Finished Lab01

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,14 +5,21 @@ public class Collatz {
 
     /** Returns the nextNumber in a Collatz sequence. */
     public static int nextNumber(int n) {
-        // TODO: Fill in this method.
-        return 1;
+        /** If the number n is odd, then return 3n+1
+         *  n is even,then return n/2
+         */
+        if(n%2==0) {
+            return n/2;   //even number will be divided by 2
+        }
+        else {
+            return 3*n+1;
+        }
     }
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 72;
         System.out.print(n + " ");
-
+        char a='a';
         // Some starter code to test
         while (n != 1) {          
             n = nextNumber(n);          
